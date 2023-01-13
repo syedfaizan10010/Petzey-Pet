25522fee7731084588aacb15b608a3edef90cd6b
SFaizan <SFaizan@OPT-LAP-0434>
2023-01-13 18:00:28 +0530

    petzey pet

---
+++ PetzetPet.Api/Program.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Azure;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.Resource;
+using Petzey.Pet.Data;
+using Petzey.Pet.Domain.Repository;
+
+namespace PetzetPet
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllers().AddXmlSerializerFormatters().AddNewtonsoftJson();
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+            builder.Services.AddSingleton<IPatient, PatientRepository>();
+            //builder.Services.AddOData();
+            builder.Services.AddOData();
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.EnableDependencyInjection();
+                endpoints.Select().OrderBy().Filter().MaxTop(100).SkipToken();
+                endpoints.MapControllers();
+            });
+            app.Run();
+        }
+    }
+}
